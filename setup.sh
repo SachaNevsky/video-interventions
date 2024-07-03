8ec067cd3a1fb7c233750d1a00c68614fb5a92f0
Alexandre Nevsky <k2037726@kcl.ac.uk>
2024-07-03 14:48:03 +0200

    small edit

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 # Creates a new .env file and creates the NEXT_PUBLIC_WS_IP environmental variable
 
-rm .env && touch .env && echo NEXT_PUBLIC_WS_IP=$(ipconfig | grep 'IPv4' | cut -d ':' -f2 | grep '192' | sed -e 's/^[ \t]*//') >> .env && npm install
+touch .env && > .env && echo NEXT_PUBLIC_WS_IP=$(ipconfig | grep 'IPv4' | cut -d ':' -f2 | grep '192' | sed -e 's/^[ \t]*//') >> .env && npm install
